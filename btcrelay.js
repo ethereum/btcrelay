4278b87b8655cf6956690cb8aa5eedd2c6a55fb8
ethers <ethereum@outlook.com>
2014-12-05 16:21:36 -0800

    struct BlockHeader

--- btcrelay.js
+++ btcrelay.js
@@ -1,27 +1,20 @@
 contract BTCRelay {
   struct BlockHeader {
-    Version
-    hashPrevBlock
-    hashMerkleRoot
-    Time
-    Bits
-    Nonce
+    uint32 version;
+    uint hashPrevBlock;
+    uint hashMerkleRoot;
+    uint32 time;
+    uint32 bits;
+    uint32 nonce;
   }
 
-  struct Block {
-     BlockHeader header;
-     uint score;
-  }
-
-  mapping (uint => Block) block;
   uint lastKnownBlock;
 
   function BTCRelay() {
-    owner = msg.sender;
 
     // currently BTC block 332895 but can update later
-    lastKnownBlock = '00000000000000000cfdd50d917943949fa708829ab70108c98cdb9f7d62339d';
-    block[lastKnownBlock].score = 40007470271.27;
+    lastKnownBlock = 0x00000000000000000cfdd50d917943949fa708829ab70108c98cdb9f7d62339d;
+    //block[lastKnownBlock].score = 40007470271.27;
     //block[lastKnownBlock].header = ...
   }
 
