f95f0fe4325af923e07e954f7d84f8dbaba249d9
ethers <ethereum@outlook.com>
2014-12-08 19:43:45 -0800

    calc target

--- btcrelay.js
+++ btcrelay.js
@@ -1,4 +1,6 @@
 contract BTCRelay {
+  uint TWO_POW_24;
+
   struct BlockHeader {
     uint32 version;
     hash hashPrevBlock;
@@ -11,6 +13,7 @@ contract BTCRelay {
   hash lastKnownBlock;
 
   function BTCRelay() {
+    TWO_POW_24 = 2 ^ 24;
 
     // currently BTC block 332895 but can update later
     lastKnownBlock = 0x00000000000000000cfdd50d917943949fa708829ab70108c98cdb9f7d62339d;
@@ -31,6 +34,15 @@ contract BTCRelay {
     header.time = time;
     header.bits = bits;
     header.nonce = nonce;
+
+    var exp = bits / TWO_POW_24; // is SDIV needed or this is already equivalent to bits >> 24;
+    var mant = bits & 0xffffff;
+    var target = (mant * slt(1, (8*(exp - 3))));
+    //target_hexstr = '%064x' % (mant * (1<<(8*(exp - 3))))
+    //target_str = target_hexstr.decode('hex')
+
+    //header = ( struct.pack("<L", ver) + prev_block.decode('hex')[::-1] +
+          //mrkl_root.decode('hex')[::-1] + struct.pack("<LLL", time_, bits, nonce))
   }
 
   // TODO: pass in BlockHeader when Solidity supports non-storage structs
@@ -41,5 +53,9 @@ contract BTCRelay {
     else {
       return false;
     }
+  }
+
+  function slt(uint n, uint x) returns (uint _) {
+    return n * (2^x);
   }
 }
