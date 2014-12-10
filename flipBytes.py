c63dc96bcd4bf2fcc377412ac91e55917ee8f719
ethers <ethereum@outlook.com>
2014-12-10 13:21:32 -0800

    flip 3 bytes works; need to extend to 32

--- flipBytes.py
+++ flipBytes.py
@@ -1,13 +1,16 @@
 data byte[32]
 
 def flipBytes(n):
+    numByte = 3
     mask = 0xff
     i = 0
-    while i < 32:
+    while i < numByte:
         b = n & mask
         b /= 2^(i*8)
-        b *= 2^((31-i)*8)
+        b *= 2^((numByte-i-1)*8)
         mask *= 256
         byte[i] = b
+        i += 1
 
-    return(byte)
+    ret = byte[0] | byte[1] | byte[2]
+    return(ret)
