92b0488df5d90db32d0325f7a295b39f0d23c3f6
ethers <ethereum@outlook.com>
2014-12-09 20:49:45 -0800

    compute target

--- btcrelay.py
+++ btcrelay.py
@@ -11,8 +11,18 @@
 # 17: score
 #
 
+shared:
+    TWO_POW_24 = 2 ^ 24
+
 init:
     self.storage[16] = 0x00000000000000000cfdd50d917943949fa708829ab70108c98cdb9f7d62339d
 
 code:
-    return
+    def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
+        exp = bits / TWO_POW_24
+        mant = bits & 0xffffff
+        target = mant * slt(1, (8*(exp - 3)))
+
+    # shift left
+    def slt(n, x):
+        return(n * 2^x)
