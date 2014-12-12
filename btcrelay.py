890e15c68b3eee85e0b30f40b01f2bc139006a34
ethers <ethereum@outlook.com>
2014-12-11 16:50:53 -0800

    add flipBytes

--- btcrelay.py
+++ btcrelay.py
@@ -25,6 +25,22 @@ def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
     mant = bits & 0xffffff
     target = mant * slt(1, (8*(exp - 3)))
 
+def flipBytes(n):
+    numByte = 32
+    mask = 0xff
+
+    result = 0
+    i = 0
+    while i < numByte:
+        b = n & mask
+        b /= 2^(i*8)
+        b *= 2^((numByte-i-1)*8)
+        mask *= 256
+        result = result | b
+        i += 1
+
+    return(result)
+
 # shift left
 def slt(n, x):
     return(n * 2^x)
