e51185a5538ab20250d94c4fe5e71bcfcfed0e1e
ethers <ethereum@outlook.com>
2014-12-11 17:34:02 -0800

    trying to get the hash 1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000000000000000

--- btcrelay.py
+++ btcrelay.py
@@ -18,12 +18,14 @@ def init():
     self.storage[16] = 0x00000000000000000cfdd50d917943949fa708829ab70108c98cdb9f7d62339d
 
 def code():
-    return(-1)
+    ret = self.slt(2,4)
+    return(ret)
 
 def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
     exp = bits / TWO_POW_24
     mant = bits & 0xffffff
     target = mant * slt(1, (8*(exp - 3)))
+
 
 def flipBytes(n):
     numByte = 32
@@ -44,3 +46,12 @@ def flipBytes(n):
 # shift left
 def slt(n, x):
     return(n * 2^x)
+
+
+def test():
+    b1 = 0x0100000081cd02ab7e569e8bcd9317e2
+    b2 = 0xfe99f2de44d49ab2b8851ba4a308000000000000e320b6c2fffc8d750423db8b
+    b3 = 0x1eb942ae710e951ed797f7affc8892b0f1fc122bc7f5d74df2b9441a42a14695
+    hash1 = sha256([b1,b2,b3], 3)
+    hash2 = sha256([hash1], 1)
+    return(hash2)
