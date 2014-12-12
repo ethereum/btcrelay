e6a2b964294f25db4905d2ef608e77050311e6a2
ethers <ethereum@outlook.com>
2014-12-11 16:31:48 -0800

    update to poc7 syntax

--- btcrelay.py
+++ btcrelay.py
@@ -11,18 +11,20 @@
 # 17: score
 #
 
-shared:
+def shared():
     TWO_POW_24 = 2 ^ 24
 
-init:
+def init():
     self.storage[16] = 0x00000000000000000cfdd50d917943949fa708829ab70108c98cdb9f7d62339d
 
-code:
-    def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
-        exp = bits / TWO_POW_24
-        mant = bits & 0xffffff
-        target = mant * slt(1, (8*(exp - 3)))
+def code():
+    return(-1)
 
-    # shift left
-    def slt(n, x):
-        return(n * 2^x)
+def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
+    exp = bits / TWO_POW_24
+    mant = bits & 0xffffff
+    target = mant * slt(1, (8*(exp - 3)))
+
+# shift left
+def slt(n, x):
+    return(n * 2^x)
