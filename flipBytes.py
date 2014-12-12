1f69a7a42554126548de52dea353dd1f1a88a6e3
ethers <ethereum@outlook.com>
2014-12-11 16:15:36 -0800

    can be done without array

--- flipBytes.py
+++ flipBytes.py
@@ -1,20 +1,15 @@
 def flipBytes(n):
-    byte = array(32)
     numByte = 32
     mask = 0xff
+
+    result = 0
     i = 0
     while i < numByte:
         b = n & mask
         b /= 2^(i*8)
         b *= 2^((numByte-i-1)*8)
         mask *= 256
-        byte[i] = b
+        result = result | b
         i += 1
 
-    ret = byte[0]
-    i = 1
-    while i < numByte:
-        ret = ret | byte[i]
-        i += 1
-
-    return(ret)
+    return(result)
