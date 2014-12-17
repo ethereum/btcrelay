57971a88ac4a9bc205488daeeb7c7502527fa810
ethers <ethereum@outlook.com>
2014-12-16 20:18:34 -0800

    compute blockHash

--- btcrelay.py
+++ btcrelay.py
@@ -63,7 +63,7 @@ def shiftRight(n, x):
 
 
 def test():
-    res = self.testHashHeader()
+    res = self.testAddBlock()
     return(res)
 
 
@@ -119,13 +119,14 @@ def __rawHashBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, non
 
 def testAddBlock():
     self.init333k()
-    blockHash = 0x000000000000000010e318d0c61da0b84246481d9cc097fda9327fe90b1538c1
     version = 2
     hashPrevBlock = 0x000000000000000008360c20a2ceff91cc8c4f357932377f48659b37bb86c759
     hashMerkleRoot = 0xf6f8bc90fd41f626705ac8de7efe7ac723ba02f6d00eab29c6fe36a757779ddd
     time = 1417792088
     bits = 0x181b7b74
     nonce = 796195988
+
+    blockHash = self.hashHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce)
 
     if hashPrevBlock != self.lastKnownBlock
         return(0)
