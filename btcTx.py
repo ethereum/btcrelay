243244636d34778cc9c8e8d3e80fb730a79d012e
ethers <ethereum@outlook.com>
2015-01-07 18:04:13 -0800

    test for decode()

--- btcTx.py
+++ btcTx.py
@@ -7,8 +7,17 @@ def read_as_int(bytez):
     self.pos += bytez
     return(self.pos)
 
+def decode(string, base):
+    return(13)
 
+
+def test_decode():
+    res = self.decode('0010', 256)
+    expected = 808464688
+    return(res == expected)
 
 def test_read_as_int():
+    # tx = '01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a010000001976a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac00000000'
+    expected = 808464688
     res = self.read_as_int(13)
     return(res)
