83ea78e179980b9fcf4d589c6f52dde5f33f021a
ethers <ethereum@outlook.com>
2015-01-15 17:54:08 -0800

    an input of 4 and the contract address correctly calls contractA with 14

--- sampleContractCallsA.py
+++ sampleContractCallsA.py
@@ -1,6 +1,5 @@
 extern sampleContractA: [contractA:i]
 
-def callsA(n):
-    x = 0x205215f022af4950618730bcfae161b28397bc41
+def callsA(n, x):
     res = x.contractA(n+10, as=sampleContractA)
     return(res)
