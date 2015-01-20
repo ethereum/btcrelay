75bb27f8a29e6c3d2e717ae99cb73d9434d367b4
ethers <ethereum@outlook.com>
2015-01-19 16:51:48 -0800

    reading output satoshis and scriptSize

--- btcTx.py
+++ btcTx.py
@@ -111,6 +111,17 @@ def txinFromBuf():
     log(seqNum)
 
 
+def txoutFromBuf():
+    satoshis = self.readUInt64LE()
+    log(satoshis)
+
+    scriptSize = self.readVarintNum()
+    log(scriptSize)
+
+    #if scriptSize > 0:
+        #todo
+
+
 # does not convert to numeric
 # make sure caller uses outsz=len*2
 def readReverse(len):
@@ -153,7 +164,7 @@ def twip():
     # {'locktime': 0, 'outs': [{'value': 5000000000, 'script': '76a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac'}], 'version': 1, 'ins': [{'script': '', 'outpoint': {'index': 0, 'hash': 'a9d4599e15b53f3eb531608ddb31f48c695c3d0b3538a6bda871e8b34f2f430c'}, 'sequence': 4294967295}]}
 
    #rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a010000001976a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac00000000")
-    rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff")
+    rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a0100000019")
     size = len(rawTx)
     bb = self.str2a(rawTx, size, outsz=size)
     self.copyToBuf(bb, size)
@@ -168,6 +179,15 @@ def twip():
 
     # todo loop numIns
     self.txinFromBuf()
+
+    numOuts = self.readVarintNum()
+    # log(numOuts)
+
+    # todo loop numOuts
+    self.txoutFromBuf()
+
+    # nLockTime = self.readUInt32LE()
+    #log(nLockTime)
 
     return(version == 1 && numIns == 1)
 
