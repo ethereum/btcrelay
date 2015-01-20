2922d4acc235dae6bf0c4b1c5023b83dc9b4eed2
ethers <ethereum@outlook.com>
2015-01-20 13:50:59 -0800

    test_getOutput1Script works

--- btcTx.py
+++ btcTx.py
@@ -297,6 +297,55 @@ def test_getOutput0Script():
     return(scriptArr:a)
 
 
+def getOutput1Script():
+    version = self.readUInt32LE()
+    # log(version)
+    # log(self.pos)
+    numIns = self.readVarintNum()
+    # log(numIns)
+    # log(self.pos)
+
+    # todo loop numIns
+    self.txinFromBuf()
+
+    numOuts = self.readVarintNum()
+
+    i = 0
+    outNum = 1
+    while i <= outNum:
+        satAndSize = self.txoutFromBuf(outsz=2)
+        i += 1
+
+    return(satAndSize:a)
+
+
+def test_getOutput1Script():
+    # rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a010000001976a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac00000000")
+    rawTx = text("01000000016d5412cdc802cee86b4f939ed7fc77c158193ce744f1117b5c6b67a4d70c046b010000006c493046022100be69797cf5d784412b1258256eb657c191a04893479dfa2ae5c7f2088c7adbe0022100e6b000bd633b286ed1b9bc7682fe753d9fdad61fbe5da2a6e9444198e33a670f012102f0e17f9afb1dca5ab9058b7021ba9fcbedecf4fac0f1c9e0fd96c4fdc200c1c2ffffffff0245a87edb080000001976a9147d4e6d55e1dffb0df85f509343451d170d14755188ac60e31600000000001976a9143bc576e6960a9d45201ba5087e39224d0a05a07988ac00000000")
+    size = len(rawTx)
+    # bb = self.str2a(rawTx, size, outsz=size)
+    # self.copyToBuf(bb, size)
+    #
+    # self.pos = 0
+    #
+    # self.getOutput0()
+
+
+
+    bb = self.str2a(rawTx, size, outsz=size)
+    self.copyToBuf(bb, size)
+
+    self.pos = 0
+
+    res = self.getOutput1Script(outsz=2)
+    #res[1] is the scriptSize
+    # log(res[1])
+    dblSize = res[1]*2
+    scriptArr = self.initFromArr(dblSize, 2, outsz=dblSize)
+
+    return(scriptArr:a)
+
+
 
 # this may not be needed so holding off on it
 # returns an array
