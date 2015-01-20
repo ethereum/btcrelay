fd40ba7c4f07da4a630b53811a2853bff12a907e
ethers <ethereum@outlook.com>
2015-01-20 15:48:38 -0800

    loop over inputs seems to work

--- btcTx.py
+++ btcTx.py
@@ -263,8 +263,10 @@ def getOutputScript(outNum):
     # log(numIns)
     # log(self.pos)
 
-    # todo loop numIns
-    self.txinFromBuf()
+    i = 0
+    while i < numIns:
+        self.txinFromBuf()        
+        i += 1
 
     numOuts = self.readVarintNum()
 
@@ -274,6 +276,29 @@ def getOutputScript(outNum):
         i += 1
 
     return(satAndSize:a)
+
+
+def test2():
+    # 3 ins, 2 outs
+    rawTx = text("0100000003d64e15b7c11f7532059fe6aacc819b5d886e3aaa602078db57cbae2a8f17cde8000000006b483045022027a176130ebf8bf49fdac27cdc83266a68b19c292b08df1be29f3d964c7e90b602210084ae66b4ff5ed342d78102ef8a4bde87b3ded06929ccaf9a8f71b137baa1816a012102270d473b083897519e5f01c47de7ac50877b6a295775f35966922b3571614370ffffffff54f0e7ded00c01082257eda035d65513b509ddbbe05fae19df0065c294822c9d010000006c493046022100f7423fdbcff22d3cd49921d0af92420d548b925bb1671dc826f15ccc5e05c3de022100d60a6178d892bcf012a79cf9e3430ab70a33b3fa2d156ecf541584e44fa83b150121036674d9607e0461b158c4b3d6368d1869e893cd122c68ebe47af253ff686f064effffffffa6155f8b449da0d3f9d2e1bc8e864c8b78615c1fa560076acaee8802d256a6dd010000006c493046022100e220318b55597c80eecccf9b84f37ab287c14277ccccd269d32f863d8d58d403022100f87818cbed15276f0d5be51aed5bd3b8dea934d6dd2244f2c3170369b96f365501210204b08466f452bb42cefc081ca1c773e26ce0a43566bd9d17b30065c1847072f4ffffffff02301bb50e000000001976a914bdb644fddd802bf7388df220279a18abdf65ebb788ac009ccf6f000000001976a914802d61e8496ffc132cdad325c9abf2e7c9ef222b88ac00000000")
+    size = len(rawTx)
+    bb = self.str2a(rawTx, size, outsz=size)
+    self.copyToBuf(bb, size)
+    self.pos = 0
+
+
+    res = self.getOutputScript(0, outsz=2)
+    #res[1] is the scriptSize
+    # log(res[1])
+    dblSize = res[1]*2
+    scriptArr = self.initFromArr(dblSize, 2, outsz=dblSize)
+    return(scriptArr:a)
+
+    hash = sha256(scriptArr, dblSize)
+    # log(hash)
+    exp = 59193746930381602221929576708645239567127131472230248524131494292030451013368 # not sure how to get this
+    return(hash == exp)
+
 
 
 def test_getOutputScript():
