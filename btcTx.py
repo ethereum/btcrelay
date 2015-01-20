58365aa9b1a784cecf88152a9aaf4b0b15f3e8bb
ethers <ethereum@outlook.com>
2015-01-19 17:03:53 -0800

    read nLockTime

--- btcTx.py
+++ btcTx.py
@@ -176,7 +176,7 @@ def twip():
     # {'locktime': 0, 'outs': [{'value': 5000000000, 'script': '76a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac'}], 'version': 1, 'ins': [{'script': '', 'outpoint': {'index': 0, 'hash': 'a9d4599e15b53f3eb531608ddb31f48c695c3d0b3538a6bda871e8b34f2f430c'}, 'sequence': 4294967295}]}
 
    #rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a010000001976a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac00000000")
-    rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a010000001976a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac")
+    rawTx = text("01000000010c432f4fb3e871a8bda638350b3d5c698cf431db8d6031b53e3fb5159e59d4a90000000000ffffffff0100f2052a010000001976a9143744841e13b90b4aca16fe793a7f88da3a23cc7188ac00000000")
     size = len(rawTx)
     bb = self.str2a(rawTx, size, outsz=size)
     self.copyToBuf(bb, size)
@@ -198,8 +198,8 @@ def twip():
     # todo loop numOuts
     self.txoutFromBuf()
 
-    # nLockTime = self.readUInt32LE()
-    #log(nLockTime)
+    nLockTime = self.readUInt32LE()
+    log(nLockTime)
 
     return(version == 1 && numIns == 1)
 
