428f765e5ef664d9dc04f3a692b319b816e648ef
ethers <ethereum@outlook.com>
2015-01-21 15:41:54 -0800

    reconfirmed that normal int works; need to figure out array

--- sampleContractCallsA.py
+++ sampleContractCallsA.py
@@ -1,5 +1,7 @@
 extern sampleContractA: [contractA:i]
 
-def callsA(n:a, x):
-    res = x.contractA(n[0]+10, as=sampleContractA)
+def callsA(n, x):  # use :a if array
+    # m = n[0]
+    m = n
+    res = x.contractA(m+10, as=sampleContractA)
     return(res)
