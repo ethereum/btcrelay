35bd52faf9aae9857130ed353d8935e4e2a41465
ethers <ethereum@outlook.com>
2015-01-22 15:00:04 -0800

    c.test(t.u.coerce_to_int(s.contract('sampleContractA.py')), [4]) works

--- serpentCallsA.py
+++ serpentCallsA.py
@@ -1,11 +1,12 @@
 extern sampleContractA: [contractA:i]
 
-def test(arr:a):
-    SAMPLEA = create('sampleContractA.py')
+def test(x, arr:a):
+    # SAMPLEA = create('sampleContractA.py')
     # p10 = 9997
 
     p10 = arr[0] + 10
-    res = SAMPLEA.contractA(p10, as=sampleContractA)
+    # res = SAMPLEA.contractA(p10, as=sampleContractA)
+    res = x.contractA(p10, as=sampleContractA)
     return(res)
 
 
