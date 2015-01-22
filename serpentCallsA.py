9365d4549676d909144b920f9d89079b6c4a8074
ethers <ethereum@outlook.com>
2015-01-22 14:44:14 -0800

    serpent calling works

---
+++ serpentCallsA.py
@@ -0,0 +1,16 @@
+extern sampleContractA: [contractA:i]
+
+def test(arr:a):
+    SAMPLEA = create('sampleContractA.py')
+    # p10 = 9997
+
+    p10 = arr[0] + 10
+    res = SAMPLEA.contractA(p10, as=sampleContractA)
+    return(res)
+
+
+#
+# extern mul2: [double]
+#
+# MUL2 = create('mul2.se')
+# return(MUL2.double(5, as=mul2))
