e382908b1912f021a068f5893d999e8c849bb8d3
ethers <ethereum@outlook.com>
2015-02-05 14:36:51 -0800

    start setting up ancestors

--- test/test_h2.py
+++ test/test_h2.py
@@ -25,8 +25,18 @@ class TestBtcTx(object):
     @slow
     # @pytest.mark.skipif(True,reason='skip')
     def test2(self):
+        self.c.initAncestorDepths()
+
         heaviest = 5
         for i in range(1, heaviest+1):
-          self.c.testStoreB(i, i-1)
+          self.c.testStoreB(i, i, i-1)
         self.c.testSetHeaviest(heaviest)
+
+        # self.c.testStoreB(30, 30, 2)
+        # self.c.testStoreB(31, 31, 30)
+        # self.c.testStoreB(32, 32, 31)
+        #
+
+        self.c.logAnc(heaviest)
+
         self.c.logBlockchainHead()
