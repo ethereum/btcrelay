57c6a0ba1a23b27d086d8e54bad46110fcb5e51c
ethers <ethereum@outlook.com>
2015-02-04 18:12:13 -0800

    add test_getOutput1Script

--- test/test_btcTx.py
+++ test/test_btcTx.py
@@ -60,6 +60,15 @@ class TestBtcTx(object):
         res = self.c.doCheckOutputScript(rawTx, len(rawTx), outNum, expHashOfOutputScript)
         assert res == [1]
 
+    def test_getOutput1Script(self):
+        # 1 ins, 1 outs
+        rawTx = ("01000000016d5412cdc802cee86b4f939ed7fc77c158193ce744f1117b5c6b67a4d70c046b010000006c493046022100be69797cf5d784412b1258256eb657c191a04893479dfa2ae5c7f2088c7adbe0022100e6b000bd633b286ed1b9bc7682fe753d9fdad61fbe5da2a6e9444198e33a670f012102f0e17f9afb1dca5ab9058b7021ba9fcbedecf4fac0f1c9e0fd96c4fdc200c1c2ffffffff0245a87edb080000001976a9147d4e6d55e1dffb0df85f509343451d170d14755188ac60e31600000000001976a9143bc576e6960a9d45201ba5087e39224d0a05a07988ac00000000")
+        outNum = 1
+        # exp hash is untested
+        expHashOfOutputScript = 115071730706014548547567659794968118611083380235397871058495281758347510448362
+        res = self.c.doCheckOutputScript(rawTx, len(rawTx), outNum, expHashOfOutputScript)
+        assert res == [1]
+
 
     @slow
     @pytest.mark.skipif(True,reason='skip')
