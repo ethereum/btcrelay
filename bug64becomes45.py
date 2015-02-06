829b2cb8fde061290722fc811e98428fc23a1382
ethers <ethereum@outlook.com>
2015-02-05 18:58:05 -0800

    bunch of logic removed and still bug

--- bug64becomes45.py
+++ bug64becomes45.py
@@ -2,14 +2,8 @@
 data block[2^256](_height, _ancestor[], _blockHeader(_prevBlock))
 
 data numAncestorDepths
-data ancestor_depths[2]
 def initAncestorDepths():
     self.numAncestorDepths = 2
-    i = 1
-    while i <= self.numAncestorDepths:
-        self.ancestor_depths[i - 1] = 4 ^ i
-        log(self.ancestor_depths[i - 1])
-        i += 1
 
 def testStoreB(number, blockHash, hashPrevBlock):
     self.block[blockHash]._blockHeader._prevBlock = hashPrevBlock
@@ -18,14 +12,7 @@ def testStoreB(number, blockHash, hashPrevBlock):
     log(self.numAncestorDepths*1000)
     i = 0
     while i < self.numAncestorDepths:
-        depth = self.ancestor_depths[i]
-        log(depth)
-        if self.block[blockHash]._height % depth == 1:
-            self.block[blockHash]._ancestor[i] = hashPrevBlock
-        else:
-            self.block[blockHash]._ancestor[i] = self.block[hashPrevBlock]._ancestor[i] # or i-1?
         i += 1
-    log(self.numAncestorDepths*1000)
 
 
 def test2():
