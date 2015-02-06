d46a953862bcba8fd9b846b6cbfe352553bf8ca8
ethers <ethereum@outlook.com>
2015-02-05 18:40:06 -0800

    removing blockheader fields now causes 16 to become 45...

--- bug64becomes45.py
+++ bug64becomes45.py
@@ -4,7 +4,7 @@ data heaviestBlock
 # highest score among all blocks (so far)
 data highScore
 
-data block[2^256](_height, _score, _ancestor[], _blockHeader(_version, _prevBlock, _mrklRoot, _time, _bits, _nonce))
+data block[2^256](_height, _score, _ancestor[], _blockHeader(_prevBlock))
 
 
 def testStoreB(number, blockHash, hashPrevBlock):
@@ -17,27 +17,12 @@ def testStoreB(number, blockHash, hashPrevBlock):
         log(3333333333333)
         log(depth)
         log(blockHash * 1000000)
-        # log(self.block[blockHash]._height % depth)
         if self.block[blockHash]._height % depth == 1:
             # log(444444444)
-            # log(self.block[blockHash]._height)
-            # log(depth)
             self.block[blockHash]._ancestor[i] = hashPrevBlock
         else:
             # log(77777777777)
             self.block[blockHash]._ancestor[i] = self.block[hashPrevBlock]._ancestor[i] # or i-1?
-
-            # if self.block[blockHash]._ancestor[i] == 45:
-                # log(88888888)
-                # log(self.block[blockHash]._height)
-                # log(depth)
-            #     log(blockHash)
-            #     log(hashPrevBlock)
-            #     log(i)
-            #     log(self.block[blockHash]._ancestor[i-2])
-            #     log(self.block[blockHash]._ancestor[i-1])
-            #     log(self.block[blockHash]._ancestor[i])
-
         i += 1
 
 def logAnc(blockHash):
