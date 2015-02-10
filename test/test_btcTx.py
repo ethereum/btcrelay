282c5bc8d50b6e0ccbc10fc526e1f23876677c17
ethers <ethereum@outlook.com>
2015-02-10 12:13:39 -0800

    had to store actual block100k: test now passes

--- test/test_btcTx.py
+++ test/test_btcTx.py
@@ -23,11 +23,11 @@ class TestBtcTx(object):
 
 
     def testHeadersFrom100K(self):
-        block100k = 0x000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506
-        self.c.testingonlySetGenesis(block100k)
-        b0 = block100k
+        block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
+        self.c.testingonlySetGenesis(block100kPrev)
 
         headers = [
+            "0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b0f2b5710",
             "0100000006e533fd1ada86391f3f6c343204b0d278d4aaec1c0b20aa27ba0300000000006abbb3eb3d733a9fe18967fd7d4c117e4ccbbac5bec4d910d900b3ae0793e77f54241b4d4c86041b4089cc9b",
             "0100000090f0a9f110702f808219ebea1173056042a714bad51b916cb6800000000000005275289558f51c9966699404ae2294730c3c9f9bda53523ce50e9b95e558da2fdb261b4d4c86041b1ab1bf93",
             "01000000aff7e0c7dc29d227480c2aa79521419640a161023b51cdb28a3b0100000000003779fc09d638c4c6da0840c41fa625a90b72b125015fd0273f706d61f3be175faa271b4d4c86041b142dca82",
@@ -35,7 +35,7 @@ class TestBtcTx(object):
             "0100000079cda856b143d9db2c1caff01d1aecc8630d30625d10e8b4b8b0000000000000b50cc069d6a3e33e3ff84a5c41d9d3febe7c770fdcc96b2c3ff60abe184f196367291b4d4c86041b8fa45d63",
             "0100000045dc58743362fe8d8898a7506faa816baed7d391c9bc0b13b0da00000000000021728a2f4f975cc801cb3c672747f1ead8a946b2702b7bd52f7b86dd1aa0c975c02a1b4d4c86041b7b47546d"
         ]
-        for i in range(6):
+        for i in range(7):
             res = self.c.storeRawBlockHeader(headers[i])
             assert res == i+2
 
@@ -58,8 +58,8 @@ class TestBtcTx(object):
         # # txBlockHash = b1
         # # expB1 = 0 == self.verifyTx(tx, proofLen, hash, path, txBlockHash)
         #
-        # # verifyTx should only return 1 for b0
-        txBlockHash = b0
+        # verifyTx should only return 1 for block100k
+        txBlockHash = 0x000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506
         res = self.c.verifyTx(tx, proofLen, hash, path, txBlockHash)
         assert res == 1
 
