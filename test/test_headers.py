8bac82939a958a4c74ae4d31b7db08d557814c39
ethers <ethereum@outlook.com>
2015-02-11 13:09:58 -0800

    testRandom2TxVerify passes; WIP testRandomTxVerify

--- test/test_headers.py
+++ test/test_headers.py
@@ -65,12 +65,46 @@ class TestBtcTx(object):
 
         block300k = 0x000000000000000082ccf8f1557c5d40b21edabb18d2d691cfbf87118bac7254
         self.c.testingonlySetGenesis(block300k)
-        for i in range(5):
-            randBlock = random.randrange(startBlockNum, startBlockNum+numBlock)
-            self.randomTxVerify(randBlock)
+
+        assert self.randomTxVerify(300001) == 1
+
+        # for i in range(5):
+        #     randBlock = random.randrange(startBlockNum, startBlockNum+numBlock)
+        #     res = self.randomTxVerify(randBlock)
+        #
+        #     # should only verify when more than 6 confirmations
+        #     if randBlock < startBlockNum+numBlock-6:
+        #         assert res == 1
+        #     else:
+        #         assert res == 0
 
 
     def testRandomTxVerify(self):
+        # block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
+        # self.c.testingonlySetGenesis(block100kPrev)
+
+        headers = [
+            "01000000d153ecc827a531652c430d8895b07f6896091967d21079630321000000000000907a54a1d714ac57d8c43c8cc5b57006032fc3a5de6d97943a1e8a552fd90e11b3211b4d4c86041bb28803e8",
+            "0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b0f2b5710",
+            "0100000006e533fd1ada86391f3f6c343204b0d278d4aaec1c0b20aa27ba0300000000006abbb3eb3d733a9fe18967fd7d4c117e4ccbbac5bec4d910d900b3ae0793e77f54241b4d4c86041b4089cc9b",
+            "0100000090f0a9f110702f808219ebea1173056042a714bad51b916cb6800000000000005275289558f51c9966699404ae2294730c3c9f9bda53523ce50e9b95e558da2fdb261b4d4c86041b1ab1bf93",
+            "01000000aff7e0c7dc29d227480c2aa79521419640a161023b51cdb28a3b0100000000003779fc09d638c4c6da0840c41fa625a90b72b125015fd0273f706d61f3be175faa271b4d4c86041b142dca82",
+            "01000000e1c5ba3a6817d53738409f5e7229ffd098d481147b002941a7a002000000000077ed2af87aa4f9f450f8dbd15284720c3fd96f565a13c9de42a3c1440b7fc6a50e281b4d4c86041b08aecda2",
+            "0100000079cda856b143d9db2c1caff01d1aecc8630d30625d10e8b4b8b0000000000000b50cc069d6a3e33e3ff84a5c41d9d3febe7c770fdcc96b2c3ff60abe184f196367291b4d4c86041b8fa45d63",
+            "0100000045dc58743362fe8d8898a7506faa816baed7d391c9bc0b13b0da00000000000021728a2f4f975cc801cb3c672747f1ead8a946b2702b7bd52f7b86dd1aa0c975c02a1b4d4c86041b7b47546d"
+        ]
+        for i in range(7):
+            res = self.c.storeRawBlockHeader(headers[i])
+            assert res == i+2
+
+        block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
+        self.c.testingonlySetGenesis(block100kPrev)
+
+        res = self.randomTxVerify(100000)
+        assert res == 1
+
+    # original
+    def testRandom2TxVerify(self):
         block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
         self.c.testingonlySetGenesis(block100kPrev)
 
@@ -89,7 +123,6 @@ class TestBtcTx(object):
 
         res = self.randomTxVerify(100000)
         assert res == 1
-
 
     def randomTxMerkleCheck(self, blocknum):
         header = get_block_header_data(blocknum)
