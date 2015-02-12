fdc0e77a16d6e58586681857b96777ddfed18bd0
ethers <ethereum@outlook.com>
2015-02-12 14:38:39 -0800

    3x speedup by some macro conversions
    flipBytes macro reduced from 56 to 40 secs
    shiftLeft and shiftRight macros then led to 19 secs

--- btcrelay.py
+++ btcrelay.py
@@ -76,7 +76,7 @@ def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
 
     # TODO other validation of block?  eg timestamp
 
-    if gt(blockHash, 0) && lt(blockHash, target):
+    if gt(blockHash, 0) && lt(blockHash, target):  #TODO should sgt and slt be used?
         self.saveAncestors(blockHash, hashPrevBlock)
 
         self.block[blockHash]._blockHeader._version = version
@@ -97,28 +97,28 @@ def storeBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
 
 
 
-def flipBytes(n, numByte):
+macro flipBytes($n, $numByte):
     mask = 0xff
 
     result = 0
     i = 0
-    while i < numByte:
-        b = n & mask
+    while i < $numByte:
+        b = $n & mask
         b = div(b, 2^(i*8))
-        b *= 2^((numByte-i-1)*8)
+        b *= 2^(($numByte-i-1)*8)
         mask *= 256
         result = result | b
         i += 1
 
-    return(result)
+    result
 
 # shift left
-def shiftLeft(n, x):
-    return(n * 2^x)
+macro shiftLeft($n, $x):
+    $n * 2^$x
 
 # shift right
-def shiftRight(n, x):
-    return(div(n, 2^x))
+macro shiftRight($n, $x):
+    div($n, 2^$x)
 
 
 def isNonceValid(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
@@ -135,34 +135,34 @@ def isNonceValid(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
 def targetFromBits(bits):
     exp = div(bits, TWO_POW_24)
     mant = bits & 0xffffff
-    target = mant * self.shiftLeft(1, (8*(exp - 3)))
+    target = mant * shiftLeft(1, (8*(exp - 3)))
     return(target)
 
 
 def hashHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
-    version = self.flipBytes(version, 4)
-    hashPrevBlock = self.flipBytes(hashPrevBlock, 32)
-    hashMerkleRoot = self.flipBytes(hashMerkleRoot, 32)
-    time = self.flipBytes(time, 4)
-    bits = self.flipBytes(bits, 4)
-    nonce = self.flipBytes(nonce, 4)
+    version = flipBytes(version, 4)
+    hashPrevBlock = flipBytes(hashPrevBlock, 32)
+    hashMerkleRoot = flipBytes(hashMerkleRoot, 32)
+    time = flipBytes(time, 4)
+    bits = flipBytes(bits, 4)
+    nonce = flipBytes(nonce, 4)
 
     hash = self.doRawHashBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce)
-    return(self.flipBytes(hash, 32))
+    return(flipBytes(hash, 32))
 
 def doRawHashBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce):
-    verPart = self.shiftLeft(version, 28*8)
-    hpb28 = self.shiftRight(hashPrevBlock, 4*8)  # 81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a3080000
+    verPart = shiftLeft(version, 28*8)
+    hpb28 = shiftRight(hashPrevBlock, 4*8)  # 81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a3080000
     b1 = verPart | hpb28
 
-    hpbLast4 = self.shiftLeft(hashPrevBlock, 28*8)  # 000000000
-    hm28 = self.shiftRight(hashMerkleRoot, 4*8)  # e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0
+    hpbLast4 = shiftLeft(hashPrevBlock, 28*8)  # 000000000
+    hm28 = shiftRight(hashMerkleRoot, 4*8)  # e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0
     b2 = hpbLast4 | hm28
 
-    hmLast4 = self.shiftLeft(hashMerkleRoot, 28*8)
-    timePart = ZEROS | self.shiftLeft(time, 24*8)
-    bitsPart = ZEROS | self.shiftLeft(bits, 20*8)
-    noncePart = ZEROS | self.shiftLeft(nonce, 16*8)
+    hmLast4 = shiftLeft(hashMerkleRoot, 28*8)
+    timePart = ZEROS | shiftLeft(time, 24*8)
+    bitsPart = ZEROS | shiftLeft(bits, 20*8)
+    noncePart = ZEROS | shiftLeft(nonce, 16*8)
     b3 = hmLast4 | timePart | bitsPart | noncePart
 
     hash1 = sha256([b1,b2,b3], chars=80)
@@ -226,10 +226,10 @@ def within6Confirms(txBlockHash):
     return(0)
 
 def concatHash(tx1, tx2):
-    left = self.flipBytes(tx1, 32)
-    right = self.flipBytes(tx2, 32)
+    left = flipBytes(tx1, 32)
+    right = flipBytes(tx2, 32)
 
     hash1 = sha256([left, right], chars=64)
     hash2 = sha256([hash1], items=1)
 
-    return(self.flipBytes(hash2, 32))
+    return(flipBytes(hash2, 32))
