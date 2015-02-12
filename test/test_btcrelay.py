0c5e9ceadcfa255798e70c68b3e4396c553c048f
ethers <ethereum@outlook.com>
2015-02-12 15:18:56 -0800

    py.test can't be used to test macros so put doRawHashBlockHeader test in a test contract

--- test/test_btcrelay.py
+++ test/test_btcrelay.py
@@ -69,32 +69,6 @@ class TestBtcTx(object):
         assert blockHash == expBlockHash
 
 
-
-    # @pytest.mark.skipif(True,reason='skip')
-    def testDoRawHashBlockHeader(self):
-        # https://en.bitcoin.it/wiki/Block_hashing_algorithm
-        version = 0x01000000
-        hashPrevBlock = 0x81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000
-        hashMerkleRoot = 0xe320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b
-        time = 0xc7f5d74d
-        bits = 0xf2b9441a
-        nonce = 0x42a14695
-
-        # these should be the intermediate b1,b2,b3 values inside doRawHashBlockHeader()
-        # b1 = 0x0100000081cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a3080000
-        # b2 = 0x00000000e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0
-        # b3 = 0xf1fc122bc7f5d74df2b9441a42a1469500000000000000000000000000000000
-        # hash1 = sha256([b1,b2,b3], chars=80)
-        # hash2 = sha256([hash1], 1)
-        # return(hash2)
-
-        res = self.c.doRawHashBlockHeader(version, hashPrevBlock, hashMerkleRoot, time, bits, nonce)
-        # log(res)
-
-        expHash = 0x1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000000000000000
-        assert res == expHash
-
-
     # @pytest.mark.skipif(True,reason='skip')
     def testIsNonceValid(self):
         ver = 2
