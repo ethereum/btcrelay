ac9295f88d9ee4d217fb2f66418affe29ed4c654
ethers <ethereum@outlook.com>
2015-02-13 16:20:41 -0800

    profiling for storing 1 and 2 headers

--- test/test_btcTx.py
+++ test/test_btcTx.py
@@ -253,15 +253,17 @@ class TestBtcTx(object):
     def storeGenesisBlock(self):
         blockHeaderStr = ("0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c")
         bhBinary = blockHeaderStr.decode('hex')
-        res = self.c.storeRawBlockHeader(blockHeaderStr, bhBinary)
-        assert res == 1
+        res = self.c.storeRawBlockHeader(blockHeaderStr, bhBinary, profiling=True)
+        print('GAS: '+str(res['gas']))
+        assert res['output'] == 1
 
     def storeBlock1(self):
         # block 1
         blockHeaderStr = ("010000006fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000982051fd1e4ba744bbbe680e1fee14677ba1a3c3540bf7b1cdb606e857233e0e61bc6649ffff001d01e36299")
         bhBinary = blockHeaderStr.decode('hex')
-        res = self.c.storeRawBlockHeader(blockHeaderStr, bhBinary)
-        assert res == 2
+        res = self.c.storeRawBlockHeader(blockHeaderStr, bhBinary, profiling=True)
+        print('GAS: '+str(res['gas']))
+        assert res['output'] == 2
 
 
     def testStoringHeaders(self):
