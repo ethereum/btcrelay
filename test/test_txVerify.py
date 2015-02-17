b933f9a03bbea60a0f8bacdcdeaff0f6940417d9
ethers <ethereum@outlook.com>
2015-02-17 14:05:39 -0800

    testInsufficientBTCSent

--- test/test_txVerify.py
+++ test/test_txVerify.py
@@ -123,6 +123,7 @@ class TestBtcTx(object):
             else:
                 assert res == 0
 
+    # tx[1] of block 100K sends enough BTC, so ether should be transferred
     def testRelayTx(self):
         block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
         self.c.testingonlySetGenesis(block100kPrev)
@@ -159,6 +160,45 @@ class TestBtcTx(object):
         expEtherBalance = 13
         assert userEthBalance == expEtherBalance
         assert res == 1  # adjust according to numBlock and the block that the tx belongs to
+
+
+    # tx[2] of block 100K does NOT send enough BTC, so ether should NOT be transferred
+    def testInsufficientBTCSent(self):
+        block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
+        self.c.testingonlySetGenesis(block100kPrev)
+
+        headers = [
+            "0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b0f2b5710",
+            "0100000006e533fd1ada86391f3f6c343204b0d278d4aaec1c0b20aa27ba0300000000006abbb3eb3d733a9fe18967fd7d4c117e4ccbbac5bec4d910d900b3ae0793e77f54241b4d4c86041b4089cc9b",
+            "0100000090f0a9f110702f808219ebea1173056042a714bad51b916cb6800000000000005275289558f51c9966699404ae2294730c3c9f9bda53523ce50e9b95e558da2fdb261b4d4c86041b1ab1bf93",
+            "01000000aff7e0c7dc29d227480c2aa79521419640a161023b51cdb28a3b0100000000003779fc09d638c4c6da0840c41fa625a90b72b125015fd0273f706d61f3be175faa271b4d4c86041b142dca82",
+            "01000000e1c5ba3a6817d53738409f5e7229ffd098d481147b002941a7a002000000000077ed2af87aa4f9f450f8dbd15284720c3fd96f565a13c9de42a3c1440b7fc6a50e281b4d4c86041b08aecda2",
+            "0100000079cda856b143d9db2c1caff01d1aecc8630d30625d10e8b4b8b0000000000000b50cc069d6a3e33e3ff84a5c41d9d3febe7c770fdcc96b2c3ff60abe184f196367291b4d4c86041b8fa45d63",
+            "0100000045dc58743362fe8d8898a7506faa816baed7d391c9bc0b13b0da00000000000021728a2f4f975cc801cb3c672747f1ead8a946b2702b7bd52f7b86dd1aa0c975c02a1b4d4c86041b7b47546d"
+        ]
+        blockHeaderBinary = map(lambda x: x.decode('hex'), headers)
+        for i in range(7):
+            res = self.c.storeRawBlockHeader(headers[i], blockHeaderBinary[i])
+            assert res == i+2
+
+        # block 100000
+        header = {'nonce': 274148111, 'hash': u'000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506', 'timestamp': 1293623863, 'merkle_root': u'f3e94742aca4b5ef85488dc37c06c3282295ffec960994b2c0d5ac2a25a95766', 'version': 1, 'prevhash': u'000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250', 'bits': 453281356}
+        hashes = [u'8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87', u'fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4', u'6359f0868171b1d194cbee1af2f16ea598ae8fad666d9b012c8ed2b79a236ec4', u'e9a66845e05d5abc0ad04ec80f774a7e585c6e8db975962d069a522137b80c1d']
+
+        # tx 6359f0868171b1d194cbee1af2f16ea598ae8fad666d9b012c8ed2b79a236ec4
+        txStr = '0100000001c33ebff2a709f13d9f9a7569ab16a32786af7d7e2de09265e41c61d078294ecf010000008a4730440220032d30df5ee6f57fa46cddb5eb8d0d9fe8de6b342d27942ae90a3231e0ba333e02203deee8060fdc70230a7f5b4ad7d7bc3e628cbe219a886b84269eaeb81e26b4fe014104ae31c31bf91278d99b8377a35bbce5b27d9fff15456839e919453fc7b3f721f0ba403ff96c9deeb680e5fd341c0fc3a7b90da4631ee39560639db462e9cb850fffffffff0240420f00000000001976a914b0dcbf97eabf4404e31d952477ce822dadbe7e1088acc060d211000000001976a9146b1281eec25ab4e1e0793ff4e08ab1abb3409cd988ac00000000'
+
+        BTC_ETH = self.s.abi_contract('btc-eth.py', endowment=2000*self.ETHER)
+        res = self.doRelayTx(txStr, header, hashes, 2, BTC_ETH.address, profiling=True)
+
+        #TODO needs update
+        ethAddrBin = '948c765a6914d43f2a7ac177da2c2f6b52de3d7c'.decode('hex')
+
+        userEthBalance = self.s.block.get_balance(ethAddrBin)
+        print('USER ETH BALANCE: '+str(userEthBalance))
+        expEtherBalance = 0
+        assert userEthBalance == expEtherBalance
+        assert res == 0  # adjust according to numBlock and the block that the tx belongs to
 
 
     def test7BlockValidTx(self):
