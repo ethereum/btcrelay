00f9f7b7a8659ef505c449339567dbffaf3e900d
ethers <ethereum@outlook.com>
2015-02-27 13:43:30 -0800

    contractCallsA works using ethereum.js; next step is to add a send() inside contractA and also make this sample behave more like the btc relay

--- sampleContractCallsA.py
+++ sampleContractCallsA.py
@@ -1,10 +1,5 @@
-extern sampleContractA: [contractA:i]
+extern sampleContractA: [processTransfer:i:i]
 
-def callsA(x, n:a):  # use :a if array
-    # m = n[0]
-    # p10 = m + 10
-    # m = n
-
-    p10 = 9997
-    res = x.contractA(p10, as=sampleContractA)
+def callsA(x, n):
+    res = x.processTransfer(n)
     return(res)
