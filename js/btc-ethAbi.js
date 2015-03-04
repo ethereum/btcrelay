119128ff8fbe5ff97516860dc8d5c859b56e1103
ethers <ethereum@outlook.com>
2015-03-03 18:03:33 -0800

    relayTx on auto deployed btceth contract is passing

--- js/btc-ethAbi.js
+++ js/btc-ethAbi.js
@@ -1,25 +1,13 @@
 window.btcToEthAbi = [{
-    "name": "__getMetaForOutput(int256)",
+    "name": "getFirst2Outputs(string)",
     "type": "function",
-    "inputs": [{ "name": "outNum", "type": "int256" }],
-    "outputs": [{ "name": "out", "type": "int256[]" }]
+    "inputs": [{ "name": "txStr", "type": "string" }],
+    "outputs": [{ "name": "unknown_out", "type": "int256[]" }]
 },
 {
-    "name": "__setupForParsing(string)",
+    "name": "getUnsignedBitsLE(string,int256,int256)",
     "type": "function",
-    "inputs": [{ "name": "hexStr", "type": "string" }],
-    "outputs": []
-},
-{
-    "name": "doCheckOutputScript(string,int256,int256,int256)",
-    "type": "function",
-    "inputs": [{ "name": "rawTx", "type": "string" }, { "name": "size", "type": "int256" }, { "name": "outNum", "type": "int256" }, { "name": "expHashOfOutputScript", "type": "int256" }],
-    "outputs": [{ "name": "out", "type": "int256" }]
-},
-{
-    "name": "parseTransaction(string,int256,int256)",
-    "type": "function",
-    "inputs": [{ "name": "rawTx", "type": "string" }, { "name": "size", "type": "int256" }, { "name": "outNum", "type": "int256" }],
+    "inputs": [{ "name": "txStr", "type": "string" }, { "name": "pos", "type": "int256" }, { "name": "bits", "type": "int256" }],
     "outputs": [{ "name": "out", "type": "int256[]" }]
 },
 {
@@ -27,34 +15,4 @@ window.btcToEthAbi = [{
     "type": "function",
     "inputs": [{ "name": "txStr", "type": "string" }],
     "outputs": [{ "name": "out", "type": "int256" }]
-},
-{
-    "name": "readSimple(int256)",
-    "type": "function",
-    "inputs": [{ "name": "len", "type": "int256" }],
-    "outputs": [{ "name": "out", "type": "string" }]
-},
-{
-    "name": "readUnsignedBitsLE(int256)",
-    "type": "function",
-    "inputs": [{ "name": "bits", "type": "int256" }],
-    "outputs": [{ "name": "out", "type": "int256" }]
-},
-{
-    "name": "readVarintNum()",
-    "type": "function",
-    "inputs": [],
-    "outputs": [{ "name": "out", "type": "int256" }]
-},
-{
-    "name": "txinParse()",
-    "type": "function",
-    "inputs": [],
-    "outputs": []
-},
-{
-    "name": "txoutParse()",
-    "type": "function",
-    "inputs": [],
-    "outputs": [{ "name": "out", "type": "int256[]" }]
 }]
