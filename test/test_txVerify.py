875a5783a1f9628d6f2a69eb2cefd0312fd8840d
ethers <ethereum@outlook.com>
2015-03-06 22:12:15 -0800

    relayTx tests need to set the btc addr

--- test/test_txVerify.py
+++ test/test_txVerify.py
@@ -111,6 +111,7 @@ class TestTxVerify(object):
     def testRelayTx(self):
         BTC_ETH = self.s.abi_contract('btc-eth.py', endowment=2000*self.ETHER)
         BTC_ETH.setTrustedBtcRelay(self.c.address)
+        BTC_ETH.testingonlySetBtcAddr(0xc398efa9c392ba6013c5e04ee729755ef7f58b32)
 
         #
         # store block headers
@@ -161,6 +162,9 @@ class TestTxVerify(object):
         BTC_ETH = self.s.abi_contract('btc-eth.py', endowment=2000*self.ETHER)
         BTC_ETH.setTrustedBtcRelay(self.c.address)
 
+        # TODO update this to match tx index 2
+        BTC_ETH.testingonlySetBtcAddr(0xc398efa9c392ba6013c5e04ee729755ef7f58b32)
+
         block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
         self.c.testingonlySetGenesis(block100kPrev)
 
@@ -199,6 +203,7 @@ class TestTxVerify(object):
     def testEachConfirmation(self):
         BTC_ETH = self.s.abi_contract('btc-eth.py', endowment=2000*self.ETHER)
         BTC_ETH.setTrustedBtcRelay(self.c.address)
+        BTC_ETH.testingonlySetBtcAddr(0xc398efa9c392ba6013c5e04ee729755ef7f58b32)
 
         # tx[1] fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4
         txStr = '0100000001032e38e9c0a84c6046d687d10556dcacc41d275ec55fc00779ac88fdf357a187000000008c493046022100c352d3dd993a981beba4a63ad15c209275ca9470abfcd57da93b58e4eb5dce82022100840792bc1f456062819f15d33ee7055cf7b5ee1af1ebcc6028d9cdb1c3af7748014104f46db5e9d61a9dc27b8d64ad23e7383a4e6ca164593c2527c038c0857eb67ee8e825dca65046b82c9331586c82e0fd1f633f25f87c161bc6f8a630121df2b3d3ffffffff0200e32321000000001976a914c398efa9c392ba6013c5e04ee729755ef7f58b3288ac000fe208010000001976a914948c765a6914d43f2a7ac177da2c2f6b52de3d7c88ac00000000'
