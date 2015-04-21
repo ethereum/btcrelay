177b0f4d042093d864e98f051fbc76a226f8e809
ethers <ethereum@outlook.com>
2015-04-21 13:29:31 -0700

    get txcount pending

--- misc/693.sh
+++ misc/693.sh
@@ -4,7 +4,7 @@
 
 a=0
 
-while [ $a -lt 10 ]
+while [ $a -lt 2 ]
 do
    echo $a
    curl -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{
@@ -14,6 +14,11 @@ do
      "gasPrice": "0x9184e72a000",
      "value": "0x1"
    }
+   ],"id":1}' http://localhost:8080
+
+   curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionCount","params":[
+    "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+    "pending"
    ],"id":1}' http://localhost:8080
 
    a=`expr $a + 1`
