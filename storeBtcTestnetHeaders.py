8b8d0135988a3b4339cc117414db0e5cd1af7ad6
ethers <ethereum@outlook.com>
2015-04-23 12:34:07 -0700

    start script for storing btc testnet headers

---
+++ storeBtcTestnetHeaders.py
@@ -0,0 +1,170 @@
+from datetime import datetime, date
+
+from pyepm import api, config
+from bitcoin import *
+
+
+api_config = config.read_config()
+instance = api.Api(api_config)
+
+# CPP
+instance.address = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826"
+to = "0xfd7a5d5349a2b69a817868f206346990af2a9e87"
+
+# GO
+# instance.address = "0x9dc2299a76b68b7ffa9e3ba0fd8cd7646d21d409"
+# to = "0x0fd51f042310093b9d8df57d37a42c3523537a99"
+
+
+def main():
+    genesisNum = 350540
+    bhJson = blockr_get_block_header_data(genesisNum, network='testnet')
+    bhStr = serialize_header(bhJson)
+    print("@@@ {0}: {1}").format(genesisNum, bhStr)
+
+
+def main222():
+    initialSkip = True
+    # currHead = 0x00000000184d61d43e667b4aebe6224e0a3265a2be87048b7924d7339de6095d
+    currHead = getBlockchainHead()
+
+    chunkSize = 5
+    numHeader = 80000
+    nIter = numHeader / chunkSize
+
+    strings = ""
+    i = 0
+    nTime = 0
+
+    startTime = datetime.now().time()
+    print("@@@ START: %s" % startTime)
+
+    with open("test/headers/bh80k.txt") as f:
+        for header in f:
+
+            if initialSkip:
+                bhBinary = header[:-1].decode('hex')  # [:-1] to remove trailing \n
+                headerInt = int(bin_dbl_sha256(bhBinary[-80:])[::-1].encode('hex'), 16)
+                if headerInt == currHead:
+                    initialSkip = False
+                continue;
+
+
+            i += 1
+            strings += header[:-1]  # [:-1] to remove trailing \n
+
+            if i % chunkSize == 0:
+                storeHeaders(strings.decode('hex'), chunkSize)
+                strings = ""
+                nTime += 1
+
+                if nTime % 5 == 0:
+                    chainHead = getBlockchainHead()
+                    print('@@@ hexHead: %s' % blockHashHex(chainHead))
+
+            if nTime == nIter:
+                break
+
+    chainHead = getBlockchainHead()
+    print('@@@ DONE hexHead: %s' % blockHashHex(chainHead))
+
+
+    endTime = datetime.now().time()
+    duration = datetime.combine(date.today(), endTime) - datetime.combine(date.today(), startTime)
+    print("********** duration: "+str(duration)+" ********** start:"+str(startTime)+" end:"+str(endTime))
+
+
+def storeHeaders(bhBinary, chunkSize):
+
+    txCount = instance.transaction_count(defaultBlock='pending')
+    print('----------------------------------')
+    print('txCount: %s' % txCount)
+
+    hashOne = blockHashHex(int(bin_dbl_sha256(bhBinary[:80])[::-1].encode('hex'), 16))
+    hashLast = blockHashHex(int(bin_dbl_sha256(bhBinary[-80:])[::-1].encode('hex'), 16))
+    print('hashOne: %s' % hashOne)
+    print('hashLast: %s' % hashLast)
+
+    firstH = bhBinary[:80].encode('hex')
+    lastH = bhBinary[-80:].encode('hex')
+    print('firstH: %s' % firstH)
+    print('lastH: %s' % lastH)
+
+
+    fun_name = "bulkStoreHeader"
+    sig = "si"
+
+    # bhBinary = '\x02\x00\x00\x00~\xf0U\xe1gM.eQ\xdb\xa4\x1c\xd2\x14\xde\xbb\xee4\xae\xb5D\xc7\xecg\x00\x00\x00\x00\x00\x00\x00\x00\xd3\x99\x89c\xf8\x0c[\xabC\xfe\x8c&"\x8e\x98\xd00\xed\xf4\xdc\xbeH\xa6f\xf5\xc3\x9e-z\x88\\\x91\x02\xc8mSl\x89\x00\x19Y:G\r\x02\x00\x00\x00Tr\xac\x8b\x11\x87\xbf\xcf\x91\xd6\xd2\x18\xbb\xda\x1e\xb2@]|U\xf1\xf8\xcc\x82\x00\x00\x00\x00\x00\x00\x00\x00\xab\n\xaa7|\xa3\xf4\x9b\x15E\xe2\xaek\x06g\xa0\x8fB\xe7-\x8c$\xae#q@\xe2\x8f\x14\xf3\xbb|k\xccmSl\x89\x00\x19\xed\xd8<\xcf\x02\x00\x00\x00\xa9\xab\x12\xe3,\xed\xdc+\xa5\xe6\xade\x1f\xacw,\x986\xdf\x83M\x91\xa0I\x00\x00\x00\x00\x00\x00\x00\x00\xdfuu\xc7\x8f\x83\x1f \xaf\x14~\xa7T\xe5\x84\xaa\xd9Yeiic-\xa9x\xd2\xddq\x86#\xfd0\xc5\xccmSl\x89\x00\x19\xe6Q\x07\xe9\x02\x00\x00\x00,P\x1f\xc0\xb0\xfd\xe9\xb3\xc1\x0e#S\xc1TI*5k\x1a\x02)^+\x86\x00\x00\x00\x00\x00\x00\x00\x00\xa7\xaaa\xc8\xd3|\x88v\xba\xa0\x17\x9ej2\x94D4\xbf\xd3\xe1\xccug\x89*1K\x0c{\x9e]\x92\'\xcemSl\x89\x00\x19\xa4\xa0<{\x02\x00\x00\x00\xe7\xfc\x91>+y\n0v\x0c\xaa\xfb\x9b_\xaa\xe1\xb5\x1dlT\xff\xe4\xae\x82\x00\x00\x00\x00\x00\x00\x00\x00P\xad\x11k\xfb\x11c\x03\x03a\xd9}H\xb4\xca\x90\'\xa4\x9b\xca\xf8\xb8\xd4!\x1b\xaa\x92\xccr\xe7\xe1#f\xcfmSl\x89\x00\x19\xe6\x13\x9c\x82'
+    data = [bhBinary, chunkSize]
+
+    gas = 3000000
+    gas_price = 10000000000000  # if CPP, may need to make this 10szabo (10e12)
+    value = 0
+
+
+    #
+    # store the headers
+    #
+    wait = True
+    from_count = instance.transaction_count(defaultBlock='pending')
+    if wait:
+        from_block = instance.last_block()
+
+    instance.transact(to, fun_name=fun_name, sig=sig, data=data, gas=gas, gas_price=gas_price, value=value)
+
+    # instance.wait_for_transaction(
+    #     from_count=from_count,
+    #     #verbose=(True if api_config.get('misc', 'verbosity') > 1 else False))
+    #     verbose=True)
+
+    if wait:
+        for i in range(1):
+            instance.wait_for_next_block(from_block=from_block,
+                #verbose=(True if api_config.get('misc', 'verbosity') > 1 else False))
+                verbose=True)
+            from_block = instance.last_block()
+
+
+    # 2nd try
+    # instance.transact(to, fun_name=fun_name, sig=sig, data=data, gas=gas, gas_price=gas_price, value=value)
+    # instance.wait_for_transaction(
+    #     from_count=from_count,
+    #     #verbose=(True if api_config.get('misc', 'verbosity') > 1 else False))
+    #     verbose=True)
+    #
+    # if wait:
+    #     instance.wait_for_next_block(from_block=from_block,
+    #         #verbose=(True if api_config.get('misc', 'verbosity') > 1 else False))
+    #         verbose=True)
+
+
+
+    chainHead = getBlockchainHead()
+    expHead = int(bin_dbl_sha256(bhBinary[-80:])[::-1].encode('hex'), 16)
+
+    if chainHead != expHead:
+        print('@@@@@ MISMATCH chainHead: {0} expHead: {1}').format(
+            blockHashHex(chainHead), blockHashHex(expHead))
+        # sys.exit(1)
+
+
+def getBlockchainHead():
+    gas = 3000000
+    gas_price = 1
+
+    fun_name = 'getBlockchainHead'
+    sig = ''
+    data = []
+
+    callResult = instance.call(to, fun_name=fun_name, sig=sig, data=data, gas=gas, gas_price=gas_price)
+    chainHead = callResult[0] if len(callResult) else callResult
+    return chainHead
+
+
+def blockHashHex(number):
+    hexHead = hex(number)[2:-1] # snip off the 0x and trailing L
+    hexHead = '0'*(64-len(hexHead)) + hexHead
+    return hexHead
+
+if __name__ == '__main__':
+    main()
