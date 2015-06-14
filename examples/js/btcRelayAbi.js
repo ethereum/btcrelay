045a892a2c4a2b1b19903859d3f985e307dd0db4
ethers <ethereum@outlook.com>
2015-06-14 02:54:06 -0700

    remove functions that shouldn't be public

--- examples/js/btcRelayAbi.js
+++ examples/js/btcRelayAbi.js
@@ -41,38 +41,14 @@ window.btcRelayAbi = [{
     "outputs": [{ "name": "out", "type": "int256" }]
 },
 {
-    "name": "saveAncestors(int256,int256)",
-    "type": "function",
-    "inputs": [{ "name": "blockHash", "type": "int256" }, { "name": "hashPrevBlock", "type": "int256" }],
-    "outputs": []
-},
-{
-    "name": "setInitialParent(int256,int256,int256)",
-    "type": "function",
-    "inputs": [{ "name": "blockHash", "type": "int256" }, { "name": "height", "type": "int256" }, { "name": "cumulativeDifficulty", "type": "int256" }],
-    "outputs": [{ "name": "out", "type": "int256" }]
-},
-{
     "name": "storeBlockHeader(bytes)",
     "type": "function",
     "inputs": [{ "name": "blockHeaderBinary", "type": "bytes" }],
     "outputs": [{ "name": "out", "type": "int256" }]
 },
 {
-    "name": "testingonlySetHeaviest(int256)",
-    "type": "function",
-    "inputs": [{ "name": "blockHash", "type": "int256" }],
-    "outputs": []
-},
-{
     "name": "verifyTx(int256,int256,int256[],int256)",
     "type": "function",
     "inputs": [{ "name": "txHash", "type": "int256" }, { "name": "txIndex", "type": "int256" }, { "name": "sibling", "type": "int256[]" }, { "name": "txBlockHash", "type": "int256" }],
-    "outputs": [{ "name": "out", "type": "int256" }]
-},
-{
-    "name": "within6Confirms(int256)",
-    "type": "function",
-    "inputs": [{ "name": "txBlockHash", "type": "int256" }],
     "outputs": [{ "name": "out", "type": "int256" }]
 }]
