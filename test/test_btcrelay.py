e31c38e3b3b19743ad5a5855a77bc1bc2a50fc91
ethers <ethereum@outlook.com>
2015-10-02 19:46:40 -0700

    lower gas limit by separating out macro tests

--- test/test_btcrelay.py
+++ test/test_btcrelay.py
@@ -16,7 +16,7 @@ class TestBtcRelay(object):
     ETHER = 10 ** 18
 
     def setup_class(cls):
-        tester.gas_limit = int(2.95e6)  # include costs of debug methods
+        tester.gas_limit = int(2.35e6)  # include costs of debug methods
         cls.s = tester.state()
         cls.c = cls.s.abi_contract(cls.CONTRACT_DEBUG, endowment=2000*cls.ETHER)
         cls.snapshot = cls.s.snapshot()
@@ -337,19 +337,6 @@ class TestBtcRelay(object):
         print('GAS: %s' % res['gas'])
         assert res['output'] == bhBytes
 
-        assert self.c.funcPrevBlock(blockHash) == block300K
-
-        assert self.c.funcGetBits(blockHash) == 0x181b7b74
-
-
-    # was converted to macro
-    # def testFastHashBlock(self):
-    #     blockHeaderStr = "0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b0f2b5710"
-    #     bhBytes = blockHeaderStr.decode('hex')
-    #     res = self.c.fastHashBlock(bhBytes, profiling=True)
-    #     print('GAS: '+str(res['gas']))
-    #     exp = 0x000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506
-    #     assert res['output'] == exp
 
     def testComputeMerkle(self):
         # values are from block 100K
@@ -368,24 +355,3 @@ class TestBtcRelay(object):
     def testsetInitialParentOnlyOnce(self):
         assert self.c.setInitialParent(0, 0, 1) == 1
         assert self.c.setInitialParent(0, 0, 1) == 0
-
-
-    #
-    # macro tests start
-    #
-
-    def testTargetFromBits(self):
-        bits = 0x19015f53
-        exp = 8614444778121073626993210829679478604092861119379437256704
-        res = self.c.funcTargetFromBits(bits)
-        assert res == exp
-
-    def testConcatHash(self):
-        tx1 = 0x8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87
-        tx2 = 0xfff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4
-        r = self.c.funcConcatHash(tx1, tx2)
-        assert r % 2**256 == 0xccdafb73d8dcd0173d5d5c3c9a0770d0b3953db889dab99ef05b1907518cb815
-
-    #
-    # end of macro tests
-    #
