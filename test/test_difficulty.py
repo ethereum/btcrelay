600fc84653e7a70d2cf29ddf75f3dac20825170b
ethers <ethereum@outlook.com>
2015-10-09 01:21:34 -0700

    special case for getBlockHash(0). now need to align test data
    testDifficultyAdjust is failing to store the 5th header, because 5th
    header should have different bits, since we're testing
    DIFFICULTY_ADJUSTMENT_INTERVAL of 5

--- test/test_difficulty.py
+++ test/test_difficulty.py
@@ -18,7 +18,7 @@ class TestDifficulty(object):
     ETHER = 10 ** 18
 
     def setup_class(cls):
-        tester.gas_limit = int(3.1e6)  # include costs of debug methods
+        tester.gas_limit = int(4.1e6)  # include costs of debug methods
         cls.s = tester.state()
         cls.c = cls.s.abi_contract(cls.CONTRACT_DEBUG, endowment=2000*cls.ETHER)
         cls.snapshot = cls.s.snapshot()
@@ -64,14 +64,16 @@ class TestDifficulty(object):
 
     def testDifficultyAdjust(self):
         block100kPrev = 0x000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250
-        blockPrevNum = 99999
+        blockPrevNum = 0
         self.c.setInitialParent(block100kPrev, blockPrevNum, 1)
 
         # TODO repeat this test but with 1 header, and then 7 headers?
         headers = [
             "0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b0f2b5710",
-            "0100000006e533fd1ada86391f3f6c343204b0d278d4aaec1c0b20aa27ba0300000000006abbb3eb3d733a9fe18967fd7d4c117e4ccbbac5bec4d910d900b3ae0793e77f54241b4d4c86041b4089cc9b"
-            #"0100000090f0a9f110702f808219ebea1173056042a714bad51b916cb6800000000000005275289558f51c9966699404ae2294730c3c9f9bda53523ce50e9b95e558da2fdb261b4d4c86041b1ab1bf93"
+            "0100000006e533fd1ada86391f3f6c343204b0d278d4aaec1c0b20aa27ba0300000000006abbb3eb3d733a9fe18967fd7d4c117e4ccbbac5bec4d910d900b3ae0793e77f54241b4d4c86041b4089cc9b",
+            "0100000090f0a9f110702f808219ebea1173056042a714bad51b916cb6800000000000005275289558f51c9966699404ae2294730c3c9f9bda53523ce50e9b95e558da2fdb261b4d4c86041b1ab1bf93",
+            "01000000aff7e0c7dc29d227480c2aa79521419640a161023b51cdb28a3b0100000000003779fc09d638c4c6da0840c41fa625a90b72b125015fd0273f706d61f3be175faa271b4d4c86041b142dca82",
+            "01000000e1c5ba3a6817d53738409f5e7229ffd098d481147b002941a7a002000000000077ed2af87aa4f9f450f8dbd15284720c3fd96f565a13c9de42a3c1440b7fc6a50e281b4d4c86041b08aecda2"
         ]
         blockHeaderBytes = map(lambda x: x.decode('hex'), headers)
         for i in range(len(headers)):
@@ -82,11 +84,12 @@ class TestDifficulty(object):
             # print('@@@@ real chain score: ' + str(self.c.getCumulativeDifficulty()))
             assert res == i+1+blockPrevNum
 
+        assert 0
+
         cumulDiff = self.c.getCumulativeDifficulty()
 
         height = self.c.getLastBlockHeight()
-        print 'height: ' + str(height)
-
+        print '@@@ height: ' + str(height)
 
         # insert block with lower difficulty
         # using script/mine.py (commit 3908709) this block
