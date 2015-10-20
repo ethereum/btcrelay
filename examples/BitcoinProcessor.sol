17e3c072c14fc480b9000519b4b16657a67a92a3
ethers <ethereum@outlook.com>
2015-10-20 01:00:58 -0700

    todo about verifying msg.sender

--- examples/BitcoinProcessor.sol
+++ examples/BitcoinProcessor.sol
@@ -3,6 +3,10 @@ contract BitcoinProcessor {
 
     function processTransaction(bytes txn, uint256 txHash) returns (int256) {
         log0("processTransaction called");
+
+        // TODO this is a sample and there should be a check here that msg.sender
+        // is the btcrelay contract
+
         log0(bytes32(txHash));
         lastTxHash = txHash;
         // parse & do whatever with txn
