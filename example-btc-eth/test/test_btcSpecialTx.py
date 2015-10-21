961a6b966e34f587b31f1584a8abaee2bacdd562
ethers <ethereum@outlook.com>
2015-10-21 00:07:49 -0700

    testGetUnsignedBitsLE

--- example-btc-eth/test/test_btcSpecialTx.py
+++ example-btc-eth/test/test_btcSpecialTx.py
@@ -17,6 +17,10 @@ class TestBtcSpecialTx(object):
 
     ETHER = 10 ** 18
 
+    TX_STR = '010000000158115acce0e68bc58ecb89e6452380bd68da56dc0a163d9806c04b24dfefe269000000008a47304402207a0bf036d5c78d6910d608c47c9e59cbf5708df51fd22362051b8f1ecd9691d1022055ee6ace9f12f02720ce91f62916570dbd93b2aa1e91be7da8e5230f62606db7014104858527cb6bf730cbd1bcf636bc7e77bbaf0784b9428ec5cca2d8378a0adc75f5ca893d14d9db2034cbb7e637aacf28088a68db311ff6f1ebe6d00a62fed9951effffffff0210980200000000001976a914a0dc485fc3ade71be5e1b68397abded386c0adb788ac10270000000000001976a914d3193ccb3564d5425e4875fe763e26e2fce1fd3b88ac00000000'
+    # TX_STR = '01 00 00 00 01 58115acce0e68bc58ecb89e6452380bd68da56dc0a163d9806c04b24dfefe269000000008a47304402207a0bf036d5c78d6910d608c47c9e59cbf5708df51fd22362051b8f1ecd9691d1022055ee6ace9f12f02720ce91f62916570dbd93b2aa1e91be7da8e5230f62606db7014104858527cb6bf730cbd1bcf636bc7e77bbaf0784b9428ec5cca2d8378a0adc75f5ca893d14d9db2034cbb7e637aacf28088a68db311ff6f1ebe6d00a62fed9951effffffff0210980200000000001976a914a0dc485fc3ade71be5e1b68397abded386c0adb788ac10270000000000001976a914d3193ccb3564d5425e4875fe763e26e2fce1fd3b88ac00000000'
+    TX_BYTES = TX_STR.decode('hex');
+
     def setup_class(cls):
         tester.gas_limit = 2 * 10**6
         cls.s = tester.state()
@@ -27,6 +31,12 @@ class TestBtcSpecialTx(object):
     def setup_method(self, method):
         self.s.revert(self.snapshot)
         tester.seed = self.seed
+
+    def testGetUnsignedBitsLE(self):
+        assert self.c.getUnsignedBitsLE('23', 0, 8) == [1, 0x23]
+        assert self.c.getUnsignedBitsLE('45', 0, 8) == [1, 0x45]
+        assert self.c.getUnsignedBitsLE('2345', 0, 16) == [2, 0x4523]
+
 
 
     def test_testnetTx(self):
@@ -43,3 +53,7 @@ class TestBtcSpecialTx(object):
         out2ndScriptIndex = res[2]
         ethAddrIndex = out2ndScriptIndex*2 + 6
         assert txStr[ethAddrIndex:ethAddrIndex+40] == 'd3193ccb3564d5425e4875fe763e26e2fce1fd3b'
+
+    def testGetVersion(self):
+        res = self.c.parseBytes(self.TX_BYTES)
+        assert res == 1
