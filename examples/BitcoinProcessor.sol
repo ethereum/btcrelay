5e9fcd88189324733e70f60b02881550c59224ef
ethers <ethereum@outlook.com>
2015-10-20 19:57:53 -0700

    comment BitcoinProcessor

--- examples/BitcoinProcessor.sol
+++ examples/BitcoinProcessor.sol
@@ -1,3 +1,7 @@
+/*
+Example contract that can process Bitcoin transactions relayed to it via
+BTCRelay.
+ */
 contract BitcoinProcessor {
     uint256 public lastTxHash;
 
@@ -7,14 +11,14 @@ contract BitcoinProcessor {
         _trustedBTCRelay = trustedBTCRelay;
     }
 
+    // this exact function signature is required as it has to match
+    // the signature specified in BTCRelay (otherwise BTCRelay will not call it)
     function processTransaction(bytes txn, uint256 txHash) returns (int256) {
         log0("processTransaction called");
 
-        // only allow trustedBTCRelay, otherwise anyone can provide a
-        // fake txn
+        // only allow trustedBTCRelay, otherwise anyone can provide a fake txn
         if (msg.sender == _trustedBTCRelay) {
-            log1("processTransaction tHash, ", bytes32(txHash));
-            // log0(bytes32(txHash));
+            log1("processTransaction txHash, ", bytes32(txHash));
             lastTxHash = txHash;
             // parse & do whatever with txn
             return 1;
