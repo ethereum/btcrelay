0025d2c370274c6749fddddfc97ae4a430218336
ethers <ethereum@outlook.com>
2015-10-20 19:43:39 -0700

    BitcoinProcessor must specify trustedBTCRelay

--- examples/BitcoinProcessor.sol
+++ examples/BitcoinProcessor.sol
@@ -1,15 +1,26 @@
 contract BitcoinProcessor {
     uint256 public lastTxHash;
 
+    address private _trustedBTCRelay;
+
+    function BitcoinProcessor(address trustedBTCRelay) {
+        _trustedBTCRelay = trustedBTCRelay;
+    }
+
     function processTransaction(bytes txn, uint256 txHash) returns (int256) {
         log0("processTransaction called");
 
-        // TODO this is a sample and there should be a check here that msg.sender
-        // is the btcrelay contract
+        // only allow trustedBTCRelay, otherwise anyone can provide a
+        // fake txn
+        if (msg.sender == _trustedBTCRelay) {
+            log1("processTransaction tHash, ", bytes32(txHash));
+            // log0(bytes32(txHash));
+            lastTxHash = txHash;
+            // parse & do whatever with txn
+            return 1;
+        }
 
-        log0(bytes32(txHash));
-        lastTxHash = txHash;
-        // parse & do whatever with txn
-        return 1;
+        log0("processTransaction failed");
+        return 0;
     }
 }
