8baafdb685f97566b373904014804ba538d6a496
ethers <ethereum@outlook.com>
2016-02-16 00:46:52 -0800

    targetFromBits

--- test/script/cumul_difficulty.py
+++ test/script/cumul_difficulty.py
@@ -1,4 +1,17 @@
+import sys
+
 # https://en.bitcoin.it/wiki/Difficulty
+
+DIFFICULTY_1 = 0x00000000FFFF0000000000000000000000000000000000000000000000000000
+
+# based on http://www.righto.com/2014/02/bitcoin-mining-hard-way-algorithms.html#ref3
+def targetFromBits(bits):
+    exp = bits / 0x1000000  # 2**24
+    mant = bits & 0xffffff
+    return mant * 256**(exp - 3)
+
+print hex(targetFromBits(0x1b0404cb))
+sys.exit(0)
 
 with open("../headers/blockchain_headers") as f:
     f.seek(72)
