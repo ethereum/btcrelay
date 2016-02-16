2ed15732b3358d6a0196e27467afd5d7664ac8eb
ethers <ethereum@outlook.com>
2016-02-16 00:59:17 -0800

    convert to int by using hex first

--- test/script/cumul_difficulty.py
+++ test/script/cumul_difficulty.py
@@ -10,13 +10,19 @@ def targetFromBits(bits):
     mant = bits & 0xffffff
     return mant * 256**(exp - 3)
 
-print hex(targetFromBits(0x1b0404cb))
-sys.exit(0)
+# print hex(targetFromBits(0x1b0404cb))
+# sys.exit(0)
 
 with open("../headers/blockchain_headers") as f:
     f.seek(72)
     rev_diff_bits = f.read(4)
-    diff_bits = rev_diff_bits[::-1].encode('hex')
-    print diff_bits
+    diff_bits = rev_diff_bits[::-1]
+
+    diff_num = targetFromBits(int(diff_bits.encode('hex'), 16))
+
+    # diff_hex = hex(diff_num)
+    # print diff_hex
+
+    print DIFFICULTY_1 / diff_num
     # f.seek(80 * startBlock)
     # bhBytes = f.read(80 * count)
