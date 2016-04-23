b64ecfca712467a317b36647fb7bbf3cb8b6d836
ethers <ethereum@outlook.com>
2016-04-22 19:33:34 -0700

    Fix logic about when to query balance

--- fetchd/fetchd.py
+++ fetchd/fetchd.py
@@ -214,7 +214,7 @@ def fetchHeaders(chunkStartNum, chunkSize, numChunk, feeVerifyTx, feeRecipient, 
         # from the wallet.
         # CHUNK_RANGE is used when chunkSize>1 so that we ask for ETH if chunkStartNum ends in
         # ????00, ????01, ????02 to ????04
-        if ((chunkSize == 1 and chunkStartNum % 100) or
+        if ((chunkSize == 1 and chunkStartNum % 100 == 0) or
             (chunkStartNum % 100 in CHUNK_RANGE)) and useWallet:
             myWei = instance.balance_at(instance.address)
             myBalance = myWei / 1e18
