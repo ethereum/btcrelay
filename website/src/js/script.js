f6ac5cf1b734a0170cd19dbfa6a11b1882444c44
cubedro <contact@siteshop.ro>
2016-08-24 14:44:09 +0300

    updated mainnet infura host for web3 HttpProvider

--- website/src/js/script.js
+++ website/src/js/script.js
@@ -1,5 +1,5 @@
 var web3;
-var mainNetHost = 'http://frontier-lb.ether.camp';
+var mainNetHost = 'https://mainnet.infura.io:8545';
 var testNetHost = 'https://morden.infura.io:8545';
 var mainNetAddr = '0x41f274c0023f83391de4e0733c609df5a124c3d4';
 var testNetAddr = '0x5770345100a27b15f5b40bec86a701f888e8c601';
